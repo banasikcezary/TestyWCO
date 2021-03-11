85296c9d979f9f939573d77b5316b1ba5d4805bc
banasikcezary <cezary003@o2.pl>
2021-03-11 09:51:36 +0100

    aktualizacja testów

---
+++ src/test/java/Tests/MassEditStatus.java
@@ -0,0 +1,37 @@
+package Tests;
+
+import PageObject.Dialog;
+import PageObject.LoginPage;
+import PageObject.UserList;
+import org.testng.annotations.Test;
+import testng.listeners.RetryAnalyzer;
+
+public class MassEditStatus extends TestBase{
+
+    @Test(priority = 2,
+            description = "as User Try Search User On The List",
+            retryAnalyzer = RetryAnalyzer.class,
+            alwaysRun = true)
+    public void asUserTryMassEditRecOutgoing() {
+
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.typeIntoPhoneNumberField("+48690408932");
+        loginPage.typeIntoUsernameField("CRM2J2DF6ZZ");
+        loginPage.typeIntoPasswordField("Centralka123456!");
+        loginPage.clickIntoLogInButton();
+        loginPage.checkPositiveLoginToApp();
+
+        UserList userList = new UserList(driver);
+        userList.clickIntoUserListLink();
+        userList.selectAllColumns();
+        userList.clickOnEditStatus();
+        userList.setValueFromNotActiveToActiveMassChange();
+        userList.clickOnSaveBtn();
+        Dialog dialog=new Dialog(driver);
+        dialog.confirmPopup();
+
+        userList.validationValueFromActiveToNotActiveMassChange();
+
+
+    }
+}
---
+++ src/test/java/Tests/PersonalizedMessageForUserOutgoingCallsWavPlEng.java
@@ -0,0 +1,40 @@
+package Tests;
+
+import PageObject.Configuration;
+import PageObject.Dialog;
+import PageObject.LoginPage;
+import org.testng.annotations.Test;
+import testng.listeners.RetryAnalyzer;
+
+import java.awt.*;
+
+public class PersonalizedMessageForUserOutgoingCallsWavPlEng extends TestBase {
+
+    @Test(priority = 4,
+            description = "as Admin Try Create New User",
+            retryAnalyzer = RetryAnalyzer.class,
+            alwaysRun = true)
+    public void asAdminTryCreateNewBlackList() throws AWTException {
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.typeIntoPhoneNumberField("+48690408932");
+        loginPage.typeIntoUsernameField("CRM2J2DF6ZZ");
+        loginPage.typeIntoPasswordField("Orange123456!");
+        loginPage.clickIntoLogInButton();
+        loginPage.checkPositiveLoginToApp();
+
+
+        Configuration configuration = new Configuration(driver);
+        configuration.clickConfigurationLink();
+
+        configuration.clickOnFunctionalitySettings();
+        configuration.setPersonalizedMessageForUserOutgoingCalls("src/test/java/resources/sample.wav" , "src/test/java/resources/sample.wav");
+
+        configuration.clickSaveButton();
+        PageObject.Dialog dialog=new Dialog(driver);
+        dialog.confirmPopup();
+
+        configuration.verifyPersonalizedMessageForUserOutgoingCalls();
+
+
+    }
+}
