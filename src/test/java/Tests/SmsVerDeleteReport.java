084a2bdea735064f8630b6154febdbd86aa1665f
banasikcezary <cezary003@o2.pl>
2021-03-17 16:32:05 +0100

    Testy sms

---
+++ src/test/java/Tests/SmsVerDeleteReport.java
@@ -0,0 +1,84 @@
+package Tests;
+
+import PageObject.Dialog;
+import PageObject.LoginPage;
+import PageObject.ReportPage;
+import PageObject.UserAndPermissions;
+import org.testng.annotations.Test;
+import testng.listeners.RetryAnalyzer;
+
+import java.awt.*;
+import java.text.SimpleDateFormat;
+
+public class SmsVerDeleteReport extends TestBase {
+
+    @Test(priority = 2,
+            description = "as Admin Try Create New User",
+            retryAnalyzer = RetryAnalyzer.class,
+            alwaysRun = true)
+    public void asAdminTryCreateNewBlackList() throws AWTException {
+
+        driver.navigate().to("http://localhost:8075/cert-wco/");
+
+
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.typeIntoPhoneNumberField("+48690408932");
+        loginPage.typeIntoUsernameField("CRM2J2DF6ZZ");
+        loginPage.typeIntoPasswordField("Orange123456!");
+        loginPage.clickIntoLogInButton();
+        loginPage.checkPositiveLoginToApp();
+
+
+        UserAndPermissions user = new UserAndPermissions(driver);
+        user.clickOnUserAndPermissionButton();
+        user.clickOnLinkPrivilleges();
+        user.clickOnSmsVerifyDeleteReport();
+
+        loginPage.clickOnLogoutButton();
+        loginPage.typeIntoPhoneNumberField("+48690408932");
+        loginPage.typeIntoUsernameField("UserTest");
+        loginPage.typeIntoPasswordField("pass1");
+        loginPage.clickIntoLogInButton();
+        loginPage.checkPositiveLoginToApp();
+
+
+        ReportPage report=new ReportPage(driver);
+        report.clickOnReportTab();
+        report.clickOnProperty();
+        report.clickOnRecordedCallsReport();
+        report.typeInStartReportDateForRecordedCallsField();
+        report.typeInEndReportDateForRecordedCallsField();
+
+        report.clickInSelectionMainNumber10();
+        report.clickInSelectNumber10();
+
+        report.clickInGenerateRecordedCalls();
+
+
+        report.checkGenerateNewReport("Nagrane połączenia");
+        report.checkStatusNewReport();
+
+
+        String lastDate = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(System.currentTimeMillis() + 600000L);
+        System.out.println(lastDate);
+        report.clickOnReportTab();
+        int quantity =report.validateQuantityReportBefore();
+        report.deleteLastReport();
+        PageObject.Dialog dialog=new Dialog(driver);
+        dialog.clickOnConfirmButton();
+
+
+        String nowDate = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(System.currentTimeMillis() + 660000L);
+        System.out.println(nowDate);
+        DatabaseTestingDemo databaseTestingDemo = new DatabaseTestingDemo();
+        String smsKey = databaseTestingDemo.test(lastDate, nowDate);
+        System.out.println(smsKey);
+        dialog.confirmSms(smsKey);
+
+        dialog.confirmPopup();
+
+        report.validateQuantityReportAfter(quantity);
+
+
+    }
+}
